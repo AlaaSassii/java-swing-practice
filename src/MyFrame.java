59fad24814b1598b56eecc02a40f93d56fa4be75
alaasassii <alaasassijs@gmail.com>
2024-03-22 15:04:33 +0100

    add new things

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,9 +1,48 @@
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 
-
-public class MyFrame extends JFrame   {
+public class MyFrame extends JFrame  implements ActionListener {
+	JButton button ; 
+	JTextField textField;
+	MyFrame(){
+		button = new JButton("click me!!");
+		button.addActionListener(this);
 		
+		textField = new JTextField();
+		textField.setPreferredSize(new Dimension(250,40));
+		textField.setFont(new Font("Consolas",Font.PLAIN,35));
+		textField.setForeground(Color.white);
+		textField.setBackground(Color.red);
+		textField.setCaretColor(Color.white);
+		textField.setText("username");
+		
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new FlowLayout());	
+		this.add(textField);
+		this.add(button);
+		this.pack();
+		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == button) {
+			System.out.println("Welcome " + textField.getText());
+			textField.setEditable(false);
+
+		}
+	}
 
 		}
