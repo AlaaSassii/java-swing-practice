b883e9098d31cfc97fc667e4ebae97ecb2f1506d
alaasassii <alaasassijs@gmail.com>
2024-03-22 13:11:42 +0100

    add new thigns

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 class Main {
@@ -10,62 +12,27 @@ class Main {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(500,500);
-		frame.setLayout(new BorderLayout(10,10)); // lezem t7otha besh border 
+		frame.setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
+		
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(100,250));
+		panel.setBackground(Color.lightGray);
+		panel.setLayout(new FlowLayout());//zeyda mais haka ken t7b trod layout flow
+		
+		panel.add(new JButton("1"));
+		panel.add(new JButton("1"));
+		panel.add(new JButton("1"));
+		panel.add(new JButton("1"));
+		panel.add(new JButton("1"));
+		panel.add(new JButton("1"));
+		panel.add(new JButton("1"));
+		panel.add(new JButton("1"));
+		panel.add(new JButton("1"));
+		panel.add(new JButton("1"));
+		panel.add(new JButton("1"));
+		
+		frame.add(panel);
 		frame.setVisible(true);
-		
-		JPanel panel1 = new JPanel();
-		JPanel panel2 = new JPanel();
-		JPanel panel3 = new JPanel();
-		JPanel panel4 = new JPanel();
-		JPanel panel5 = new JPanel();
-		
-		JPanel panel11 = new JPanel();
-		JPanel panel22 = new JPanel();
-		JPanel panel33 = new JPanel();
-		JPanel panel44 = new JPanel();
-		JPanel panel55 = new JPanel();
-		
-		panel1.setBackground(Color.green);
-		panel2.setBackground(Color.red);
-		panel3.setBackground(Color.blue);
-		panel4.setBackground(Color.pink);
-		panel5.setBackground(Color.yellow);
-		
-		panel11.setBackground(Color.orange);
-		panel22.setBackground(Color.black);
-		panel33.setBackground(Color.gray);
-		panel44.setBackground(Color.red);
-		panel55.setBackground(Color.white);
-		
-		
-		panel1.setPreferredSize(new Dimension(100,100));
-		panel2.setPreferredSize(new Dimension(100,100));
-		panel3.setPreferredSize(new Dimension(100,100));
-		panel4.setPreferredSize(new Dimension(100,100));
-		panel5.setPreferredSize(new Dimension(100,100));
-		
-		panel5.setLayout(new BorderLayout());
-		
-		panel11.setPreferredSize(new Dimension(50,50));
-		panel22.setPreferredSize(new Dimension(50,50));
-		panel33.setPreferredSize(new Dimension(50,50));
-		panel44.setPreferredSize(new Dimension(50,50));
-		panel55.setPreferredSize(new Dimension(50,50));
-		
-		
-		panel5.add(panel11,BorderLayout.NORTH);
-		panel5.add(panel22,BorderLayout.EAST);
-		panel5.add(panel33,BorderLayout.SOUTH);
-		panel5.add(panel44,BorderLayout.WEST);
-		panel5.add(panel55,BorderLayout.CENTER);
-		
-		frame.add(panel1,BorderLayout.NORTH);
-		frame.add(panel2,BorderLayout.EAST);
-		frame.add(panel3,BorderLayout.SOUTH);
-		frame.add(panel4,BorderLayout.WEST);
-		frame.add(panel5,BorderLayout.CENTER);
-		
-	
 		
 		
 		
